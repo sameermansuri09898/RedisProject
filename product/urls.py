a2f563d347f1ba0d460e7cabe9deaa6f054275e2
sameermansuri09898 <sameermansuri09898@gmail.com>
2026-04-30 21:55:08 +0530

    update

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import result
 
 urlpatterns=[
-  path
+  path('user/',result.as_view())
 ]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from Api.models import student
+from Api.models import student,Data
 from rest_framework import generics
-from Api.studentserializer import stuserializer
+from Api.studentserializer import dataserializer
 from rest_framework.filters import SearchFilter
 class result(generics.ListAPIView):
-  queryset=student.objects.all()
-  serializer_class=  stuserializer
+  queryset=Data.objects.all()
+  serializer_class=  dataserializer
   filter_backends=[SearchFilter]
-  search_fields = ['name']
+  search_fields = ['client_name']
